e9ec0ad9135c7e3012c7baa8a7f3ab480c0e48c2
BELACHHAB-MOHAMMED <belmedbsns@gmail.com>
2021-06-03 16:53:37 +0100

    Edit the tables

--- src/layouts/partials/EdataTables.vue
+++ src/layouts/partials/EdataTables.vue
@@ -1,33 +1,46 @@
 <template>
     <div style="padding: 2%">
-        <v-simple-table fixed-header height="400px">
+        <v-simple-table
+            fixed-header
+            height="400px"
+            v-if="selected == 'Hospitalise' || selected[1] == 'Externe'"
+        >
             <template v-slot:default>
                 <thead style="background: #2ecc71; color: white">
                     <tr>
-                        <th scope="col">{{ Head.Nf }}</th>
-                        <th scope="col">{{ Head.Medicam }}</th>
-                        <th scope="col">{{ Head.Prothes }}</th>
                         <th scope="col" v-if="check == Episode">
-                            {{ Head.Categorie }}
+                            {{ Head.Type }}
                         </th>
-                        <th scope="col" v-else>{{ Head.Nature }}</th>
-                        <th scope="col" v-if="Head.Date_AD">{{ Head.Nq }}</th>
-                        <th scope="col" v-if="Head.Date_Srt">
-                            {{ Head.Totale }}
-                        </th>
+                        <th scope="col" v-else>{{ Head.Type }}</th>
+                        <th scope="col">{{ Head.info }}</th>
+                        <th scope="col">{{ Head.Ipp }}</th>
+                        <th scope="col">{{ Head.Date }}</th>
+                        <th scope="col">{{ Head.Nature }}</th>
+                        <th scope="col">{{ Head.Date_AD }}</th>
+                        <th scope="col">{{ Head.Date_Srt }}</th>
+                        <th scope="col">{{ Head.Service }}</th>
+                        <th scope="col">{{ Head.Categorie }}</th>
+                        <th scope="col">{{ Head.Type_Ad }}</th>
+                        <th scope="col">{{ Head.Tn_Ercure }}</th>
+                        <th scope="col">{{ Head.Tn_Nom }}</th>
+                        <th scope="col">Facturation</th>
                         <th scope="col">Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="item in desserts" :key="item.name">
+                    <tr v-for="data in desserts" :key="data.name">
                         <th scope="row">{{ data.Type }}</th>
-                        <td>{{ data.Nf }}</td>
-                        <td>{{ data.Medicam }}</td>
-                        <td>{{ data.Prothes }}</td>
+                        <td>{{ data.info }}</td>
+                        <td>{{ data.Ipp }}</td>
+                        <td>{{ data.date }}</td>
+                        <td>{{ data.Date_AD }}</td>
+                        <td>{{ data.Date_Srt }}</td>
+                        <td>{{ data.Service }}</td>
                         <td>{{ data.Categorie }}</td>
-                        <td>{{ data.Nature }}</td>
-                        <td>{{ data.Nq }}</td>
-                        <td>{{ data.Totale }}</td>
+                        <td>{{ data.Type_Ad }}</td>
+                        <td>{{ data.Tn_Ecure }}</td>
+                        <td>{{ data.Tn_nom }}</td>
+                        <td style="color: red">Pas Facture</td>
                         <td>
                             <Button><i class="fas fa-edit"></i></Button>
                         </td>
@@ -35,7 +48,11 @@
                 </tbody>
             </template>
         </v-simple-table>
-        <v-simple-table v-if="true == false" fixed-header height="400px">
+        <v-simple-table
+            v-if="selected == 'Externe' || selected[1] == 'Externe'"
+            fixed-header
+            height="400px"
+        >
             <template v-slot:default>
                 <thead>
                     <tr>
@@ -56,7 +73,7 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="data in allData" :key="data.name">
+                    <tr v-for="data in desserts" :key="data.name">
                         <th scope="row">{{ data.Type }}</th>
                         <td>{{ data.info }}</td>
                         <td>{{ data.Ipp }}</td>
@@ -70,18 +87,24 @@
 </template>
 <script>
 export default {
-    name: "FdataTables",
+    name: "DataTables",
     data: () => ({
         Head: {
-            Nf: "N*Facture",
-            Medicame: "Medicam",
-            Prothes: "Prothes",
+            Type: "Type",
+            Nature: "",
+            info: "Nom et prenom",
+            Ipp: "Ipp",
+            Date: "Date",
+            Date_AD: "Date_AD",
+            Date_Srt: "Date_Srt",
+            Service: "Service",
             Categorie: "Categorie",
-            Nature: "Nature",
-            Nq: "N*Quitance",
-            Totale: "Totale",
+            Type_Ad: "Type_Ad",
+            Tn_Ercure: "Tn_Ercure",
+            Tn_Nom: "Tn_Nom",
         },
         allData: [],
     }),
+    props: ["selected"],
 };
 </script>
