cbe70e3157fff0685b45110f4138551a770e8429
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2021-06-08 14:15:20 +0100

    Improve profile editing

--- src/components/AdminPanel/ProfileEdit.vue
+++ src/components/AdminPanel/ProfileEdit.vue
@@ -1,100 +1,87 @@
 <template>
-    <div class="admin-panel__home">
-        <v-card flat class="white px-10">
-            <div class="d-flex mb-8">
-                <router-link
-                    to="/admin-panel/add-agent"
-                    class="rm-underline ms-auto mr-4"
-                >
-                    <v-btn
-                        outlined
-                        class="rounded-lg text-none"
-                        color="primary"
-                    >
-                        <v-icon left size="20">mdi-plus</v-icon>
-                        Add new agent
-                    </v-btn>
-                </router-link>
-            </div>
-            <v-card-text class="pt-5">
-                <v-row no-gutters>
-                    <v-col class="me-4">
-                        <v-text-field
-                            placeholder="Prenom"
-                            label="Prenom"
-                            clearable
-                            outlined
-                            class="rounded-lg"
-                            v-model="adminData.firstName"
-                        />
-                    </v-col>
-                    <v-col>
-                        <v-text-field
-                            placeholder="Nom"
-                            label="Nom"
-                            clearable
-                            outlined
-                            class="rounded-lg"
-                            v-model="adminData.lastName"
-                        />
-                    </v-col>
-                </v-row>
-                <v-row no-gutters>
-                    <v-col cols="6">
-                        <v-text-field
-                            placeholder="Username"
-                            label="Username"
-                            clearable
-                            outlined
-                            class="rounded-lg"
-                            v-model="adminData.userName"
-                        />
-                    </v-col>
-                </v-row>
-                <v-row>
-                    <v-col class="mr-4">
-                        <v-text-field
-                            class="rounded-lg"
-                            outlined
-                            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                            :type="show1 ? 'text' : 'password'"
-                            v-model="adminData.passWord"
-                            label="Password"
-                            placeholder="Password"
-                            @click:append="show1 = !show1"
-                        />
-                    </v-col>
-                    <v-col>
-                        <v-text-field
-                            class="rounded-lg"
-                            outlined
-                            :error="repeatPassword !== adminData.passWord"
-                            :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
-                            :type="show2 ? 'text' : 'password'"
-                            v-model="repeatPassword"
-                            label="Repeat Password"
-                            placeholder="Repeat Password"
-                            @click:append="show2 = !show2"
-                        />
-                    </v-col>
-                </v-row>
-            </v-card-text>
-            <v-card-actions class="d-flex justify-center">
-                <v-btn
-                    depressed
-                    color="primary"
-                    width="160"
-                    height="40"
-                    class="text-none"
-                    :disabled="loading"
-                    :loading="loading"
-                >
-                    <v-icon left>mdi-content-save</v-icon>
-                    Save changes
+    <v-container class="px-10">
+        <div class="d-flex mb-8">
+            <router-link
+                to="/admin-panel/add-agent"
+                class="rm-underline ms-auto"
+            >
+                <v-btn outlined class="rounded-lg text-none" color="primary">
+                    <v-icon left size="20">mdi-plus</v-icon>
+                    Add new agent
                 </v-btn>
-            </v-card-actions>
-        </v-card>
-    </div>
+            </router-link>
+        </div>
+        <v-row no-gutters>
+            <v-col class="me-4">
+                <v-text-field
+                    placeholder="Prenom"
+                    label="Prenom"
+                    outlined
+                    class="rounded-lg"
+                    v-model="adminData.firstName"
+                />
+            </v-col>
+            <v-col>
+                <v-text-field
+                    placeholder="Nom"
+                    label="Nom"
+                    outlined
+                    class="rounded-lg"
+                    v-model="adminData.lastName"
+                />
+            </v-col>
+        </v-row>
+        <v-row no-gutters class="mt-2">
+            <v-col class="me-4">
+                <v-text-field
+                    placeholder="Current Password"
+                    label="Current Password"
+                    outlined
+                    class="rounded-lg"
+                    v-model="adminData.currentPassword"
+                />
+            </v-col>
+            <v-col>
+                <v-text-field
+                    class="rounded-lg"
+                    outlined
+                    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                    :type="show1 ? 'text' : 'password'"
+                    v-model="adminData.passWord"
+                    label="New Password"
+                    placeholder="New Password"
+                    @click:append="show1 = !show1"
+                />
+            </v-col>
+        </v-row>
+        <v-row no-gutters class="mt-2">
+            <v-col cols="6">
+                <v-text-field
+                    class="rounded-lg"
+                    outlined
+                    :error="repeatPassword !== adminData.passWord"
+                    :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+                    :type="show2 ? 'text' : 'password'"
+                    v-model="repeatPassword"
+                    label="Repeat New Password"
+                    placeholder="Repeat New Password"
+                    @click:append="show2 = !show2"
+                />
+            </v-col>
+        </v-row>
+        <v-btn
+            depressed
+            color="primary"
+            width="160"
+            height="40"
+            class="text-none"
+            :disabled="loading"
+            :loading="loading"
+        >
+            <v-icon left>mdi-content-save</v-icon>
+            Save changes
+        </v-btn>
+    </v-container>
 </template>
 
 <script>
