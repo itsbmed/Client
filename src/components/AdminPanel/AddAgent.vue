795e7ce6263f20f5a26d774849560e5d92c1fe17
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2021-06-08 14:14:52 +0100

    Improve add agent

--- src/components/AdminPanel/AddAgent.vue
+++ src/components/AdminPanel/AddAgent.vue
@@ -1,78 +1,73 @@
 <template>
-    <v-card flat class="white px-10">
-        <v-card-text class="pt-5">
-            <v-row no-gutters>
-                <v-col class="me-4">
-                    <v-text-field
-                        placeholder="Prenom"
-                        label="Prenom"
-                        clearable
-                        outlined
-                        class="rounded-lg"
-                        v-model="agentData.firstName"
-                    />
-                </v-col>
-                <v-col>
-                    <v-text-field
-                        placeholder="Nom"
-                        label="Nom"
-                        clearable
-                        outlined
-                        class="rounded-lg"
-                        v-model="agentData.lastName"
-                    />
-                </v-col>
-            </v-row>
-            <v-row no-gutters>
-                <v-col cols="6" class="mr-4">
-                    <v-text-field
-                        placeholder="Username"
-                        label="Username"
-                        clearable
-                        outlined
-                        class="rounded-lg"
-                        v-model="agentData.userName"
-                    />
-                </v-col>
-            </v-row>
-            <v-row>
-                <v-col class="mr-4">
-                    <v-text-field
-                        class="rounded-lg"
-                        outlined
-                        :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                        :type="show1 ? 'text' : 'password'"
-                        v-model="agentData.passWord"
-                        label="Password"
-                        placeholder="Password"
-                        @click:append="show1 = !show1"
-                    />
-                </v-col>
-                <v-col>
-                    <v-text-field
-                        class="rounded-lg"
-                        outlined
-                        :error="repeatPassword !== agentData.passWord"
-                        :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
-                        :type="show2 ? 'text' : 'password'"
-                        v-model="repeatPassword"
-                        label="Repeat Password"
-                        placeholder="Repeat Password"
-                        @click:append="show2 = !show2"
-                    />
-                </v-col>
-            </v-row>
-            <v-row no-gutters>
-                <v-col class="d-flex justify-center">
-                    <v-checkbox
-                        v-model="agentData.isAdmin"
-                        label="Admin"
-                        color="orange"
-                    ></v-checkbox>
-                </v-col>
-            </v-row>
-        </v-card-text>
-        <v-card-actions class="d-flex justify-center">
+    <v-container class="mt-8 px-10">
+        <v-row>
+            <v-col class="me-4">
+                <v-text-field
+                    placeholder="Prenom"
+                    label="Prenom"
+                    outlined
+                    class="rounded-lg"
+                    v-model="agentData.firstName"
+                />
+            </v-col>
+            <v-col>
+                <v-text-field
+                    placeholder="Nom"
+                    label="Nom"
+                    outlined
+                    class="rounded-lg"
+                    v-model="agentData.lastName"
+                />
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="6" class="mr-4">
+                <v-text-field
+                    placeholder="Username"
+                    label="Username"
+                    outlined
+                    class="rounded-lg"
+                    v-model="agentData.userName"
+                />
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col class="mr-4">
+                <v-text-field
+                    class="rounded-lg"
+                    outlined
+                    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                    :type="show1 ? 'text' : 'password'"
+                    v-model="agentData.passWord"
+                    label="Password"
+                    placeholder="Password"
+                    @click:append="show1 = !show1"
+                />
+            </v-col>
+            <v-col>
+                <v-text-field
+                    class="rounded-lg"
+                    outlined
+                    :error="repeatPassword !== agentData.passWord"
+                    :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+                    :type="show2 ? 'text' : 'password'"
+                    v-model="repeatPassword"
+                    label="Repeat Password"
+                    placeholder="Repeat Password"
+                    @click:append="show2 = !show2"
+                />
+            </v-col>
+        </v-row>
+        <v-row no-gutters>
+            <v-col class="d-flex justify-center">
+                <v-checkbox
+                    v-model="agentData.isAdmin"
+                    label="Admin"
+                    color="orange"
+                ></v-checkbox>
+            </v-col>
+        </v-row>
+        <v-row class="justify-center">
             <v-btn
                 depressed
                 color="primary"
@@ -86,8 +81,8 @@
                 <v-icon left>mdi-content-save</v-icon>
                 Add Agent
             </v-btn>
-        </v-card-actions>
-    </v-card>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
@@ -112,6 +107,8 @@ export default {
                     title: "Agent addition",
                     text: "Agent a added successfully",
                 });
+                this.agentData = {};
+                this.repeatPassword = undefined;
             } catch (err) {
                 this.$notify({
                     group: "br",
