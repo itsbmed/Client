8fd1e37014ec11b4c9d1d07977fe713f4051dae9
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2021-06-09 12:59:56 +0100

    Fix: check ipp

--- src/components/Patient/Common/Step1.vue
+++ src/components/Patient/Common/Step1.vue
@@ -135,6 +135,7 @@ export default {
         },
 
         async checkIpp() {
+            if (!this.patientData.ipp) return;
             if (this.patientData.ipp.length < 6) return;
             try {
                 let res = await this.checkExistence(this.patientData.ipp);
